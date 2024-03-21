b58a7dc37700182c78ea9153f8a176862bec8d8a
Chen Bin <chen.bin11@zte.com.cn>
2024-03-21 15:18:04 +0800

    work on test07

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
+use syn::spanned::Spanned;
 use syn::{self};
 
-#[proc_macro_derive(Builder)]
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = syn::parse_macro_input!(input as syn::DeriveInput);
     // eprintln!("{ast:#?}");
+
     let ident = ast.ident.clone();
 
     let fields = match ast {
@@ -19,6 +21,37 @@ pub fn derive(input: TokenStream) -> TokenStream {
         } => fields,
         _ => unimplemented!("derive(Builder) only supports structs with named fields"),
     };
+
+    struct FieldData {
+        id: syn::Ident,
+        ty: syn::Type,
+        nested_ty: std::option::Option<(String, syn::Type)>,
+        each_id: std::option::Option<syn::Ident>,
+    }
+
+    // for field in fields.iter() {
+    //     let id = field.ident.clone().unwrap();
+    //     let v = try_parse_builder_each(field);
+    //     let ty = field.ty.clone();
+    //     println!("{id:#}: {v:?}, {ty:?}");
+    // }
+
+    let each_builder_fields = fields.iter().map(|field| {
+        let id = field.ident.clone().unwrap();
+
+        if let Ok(Some(each)) = try_parse_builder_each(field) {
+            println!("============{each}===========");
+            quote! {
+                pub fn #each(&mut self, #each: String) -> &mut Self {
+                    self.#id.push(#each);
+                    self
+                }
+            }
+        } else {
+            println!("============fail===========");
+            quote! {}
+        }
+    });
 
     // println!("{:#?}", fields);
 
@@ -100,8 +133,14 @@ pub fn derive(input: TokenStream) -> TokenStream {
             #(#builder_fields,)*
         }
 
+        pub struct test;
+
         impl #builder_ident {
             #(#setters)*
+
+            pub struct test2;
+            #(#each_builder_fields,)*
+            pub struct test3;
 
             pub fn build(&mut self) -> std::result::Result<#ident, std::boxed::Box<dyn std::error::Error>> {
                 #(#build_checks;)*
@@ -140,4 +179,29 @@ fn try_optional(ty: &syn::Type) -> std::option::Option<syn::Type> {
     };
 
     Some(ty.clone())
+}
+
+fn try_parse_builder_each(
+    field: &syn::Field,
+) -> std::result::Result<std::option::Option<String>, syn::Error> {
+    println!("try_parse_builder_each");
+    for attr in field.attrs.iter() {
+        if attr.path().is_ident("builder") {
+            let mut value: String = String::new();
+            match attr.parse_nested_meta(|meta| {
+                if meta.path.is_ident("each") {
+                    let s: syn::LitStr = meta.value()?.parse()?;
+                    value = s.value();
+                    return std::result::Result::Ok(());
+                } else {
+                    return std::result::Result::Err(meta.error("Unrecognized attribute"));
+                }
+            }) {
+                Err(e) => return std::result::Result::Err(e),
+                Ok(()) => return std::result::Result::Ok(std::option::Option::Some(value)),
+            }
+        }
+    }
+
+    return std::result::Result::Ok(std::option::Option::None);
 }
